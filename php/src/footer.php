53b00dc6d87f46ddcb2c3b6992a9c2c4b7452074
Sinan.Karateke <sinankarateke@proton.me>
2023-07-25 13:51:36 +0200

    Bootstrap Footer mit Adressdaten und Social Media Links hinzugefügt

--- php/src/footer.php
+++ php/src/footer.php
@@ -1,4 +1,74 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      </a>
+      <a href="https://hub.docker.com/repository/docker/desertrockx" class="me-4 text-reset">
+        <i class="fab fa-docker"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/sinan-karateke-398848111" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="https://github.com/desertrockxxx" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Sinan Karateke
+          </h6>
+          <p>
+            Programmierer, Webdesigner und Systemadmin
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i>Gneisenaustr. 24, 10961 Berlin</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            sinankarateke@proton.me
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 49 176 2070 7793</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2023 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Sinan Karateke</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
 </body>
 
-<p>Copyright by Sinan Karateke</p>
 </html>
